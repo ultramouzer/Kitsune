677d67a11dcac88eda3e94ff39c3d5058557893b
Anonymous <>
2021-01-03 15:50:59 -0500

    Create log.py

---
+++ log.py
@@ -0,0 +1,14 @@
+import sys
+
+class LoggerWriter(object):
+    def __init__(self, logger, level):
+       self.logger = logger
+       self.level = level
+       self.linebuf = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.level, line.rstrip())
+
+    def flush(self):
+        pass
