95ff3af9765aff62b6bc7b2fcc9c4f347fd24530
Anonymous <>
2021-01-18 22:58:58 -0500

    Update 20210118_01_1Jlkq-add-unique-constraint-to-service-and-post-fields.py

--- migrations/20210118_01_1Jlkq-add-unique-constraint-to-service-and-post-fields.py
+++ migrations/20210118_01_1Jlkq-add-unique-constraint-to-service-and-post-fields.py
@@ -7,8 +7,44 @@ from yoyo import step
 __depends__ = {"initial"}
 
 steps = [
+    step("""
+        CREATE TABLE posts (
+            "id" varchar(255) NOT NULL,
+            "user" varchar(255) NOT NULL,
+            "service" varchar(20) NOT NULL,
+            "title" text NOT NULL DEFAULT '',
+            "content" text NOT NULL DEFAULT '',
+            "embed" jsonb NOT NULL DEFAULT '{}',
+            "shared_file" boolean NOT NULL DEFAULT '0',
+            "added" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "published" timestamp,
+            "edited" timestamp,
+            "file" jsonb NOT NULL,
+            "attachments" jsonb[] NOT NULL
+            PRIMARY KEY (id, service)
+        );
+    """, "DROP TABLE posts"),
     step(
-        "ALTER TABLE booru_posts ADD CONSTRAINT posts_pk PRIMARY KEY (id, service) ON CONFLICT DO NOTHING"
-        "ALTER TABLE booru_posts DROP CONSTRAINT posts_pk"
+        "INSERT INTO posts SELECT * FROM booru_posts ON CONFLICT DO NOTHING"
+        "INSERT INTO booru_posts SELECT * FROM posts"
+    ),
+    step(
+        "DROP TABLE booru_posts",
+        """
+        CREATE TABLE posts (
+            "id" varchar(255) NOT NULL,
+            "user" varchar(255) NOT NULL,
+            "service" varchar(20) NOT NULL,
+            "title" text NOT NULL DEFAULT '',
+            "content" text NOT NULL DEFAULT '',
+            "embed" jsonb NOT NULL DEFAULT '{}',
+            "shared_file" boolean NOT NULL DEFAULT '0',
+            "added" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "published" timestamp,
+            "edited" timestamp,
+            "file" jsonb NOT NULL,
+            "attachments" jsonb[] NOT NULL
+        );
+        """
     )
 ]
