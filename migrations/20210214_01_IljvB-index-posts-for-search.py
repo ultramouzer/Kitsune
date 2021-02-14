3ff5bf2776f0ad77b62510169a8b12e83f7781df
Anonymous <>
2021-02-14 12:44:48 -0500

    Create 20210214_01_IljvB-index-posts-for-search.py

---
+++ migrations/20210214_01_IljvB-index-posts-for-search.py
@@ -0,0 +1,15 @@
+"""
+Index posts for search
+"""
+
+from yoyo import step
+
+__depends__ = {'20210120_01_PZzeb-apply-primary-key-constraint-to-lookup-table'}
+
+steps = [
+    step(
+        "CREATE INDEX IF NOT EXISTS search_idx ON posts USING GIN (to_tsvector('english', content || ' ' || title))",
+        "DROP INDEX search_idx"
+    ),
+    step("VACUUM ANALYZE posts")
+]
