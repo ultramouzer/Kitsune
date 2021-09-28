f603f69eb8e92190e294a4f1c66aba19033ae894
Anonymous <>
2021-09-28 18:58:29 -0400

    Update download.py

--- src/internals/utils/download.py
+++ src/internals/utils/download.py
@@ -82,7 +82,8 @@ def download_file(
     discord_message_id: str = '',
     **kwargs
 ):
-    temp_dir = tempfile.mkdtemp(dir=config.download_path)
+    makedirs(join(config.download_path, 'temporary'), exist_ok=True)
+    temp_dir = tempfile.mkdtemp(dir=join(config.download_path, 'temporary'))
     temp_name = str(uuid.uuid4()) + '.temp'
     tries = 10
     for i in range(tries):
