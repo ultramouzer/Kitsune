fa7c0da73bf8d9e66d57f97c271362dee38ef085
Anonymous <>
2021-09-28 18:20:37 -0400

    Update download.py

--- src/internals/utils/download.py
+++ src/internals/utils/download.py
@@ -137,7 +137,7 @@ def download_file(
                 file.close()
                 
                 makedirs(join(config.download_path, file_hash[0:2], file_hash[2:4]), exist_ok=True)
-                rename(join(temp_dir, temp_name), join(config.download_path, hash_filename))
+                shutil.move(join(temp_dir, temp_name), join(config.download_path, hash_filename))
                 shutil.rmtree(temp_dir)
                 make_thumbnail(join(config.download_path, hash_filename))
                 return reported_filename, '/' + hash_filename, r
