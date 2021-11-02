2116d5c655da544da79739f0f01c72f95a61d8a0
Anonymous <>
2021-11-02 15:53:16 -0400

    Updated autoimport key encryption method (RSA+AES))

--- src/lib/autoimport.py
+++ src/lib/autoimport.py
@@ -1,5 +1,6 @@
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Cipher import PKCS1_OAEP, AES
+from Crypto.Random import get_random_bytes
 from ..internals.cache.redis import delete_keys, delete_keys_pattern
 from ..internals.database.database import get_raw_conn, return_conn, get_cursor
 from ..internals.utils.logger import log
@@ -7,6 +8,7 @@ from base64 import b64decode,b64encode
 import hashlib
 import config
 from joblib import Parallel, delayed
+from ..internals.cache.redis import get_redis
 
 def log_import_id(key_id, import_id):
     conn = get_raw_conn()
@@ -31,36 +33,67 @@ def kill_key(key_id):
     conn.commit()
     return_conn(conn)
 
-def decrypt_key(key, cipher):
+def decrypt_key(key, rsa_cipher, aes_cipher):
     key_to_decrypt = key
     try:
-        key_to_decrypt['decrypted_key'] = cipher.decrypt(b64decode(key_to_decrypt['encrypted_key'])).decode('utf-8')
+        key_to_decrypt['decrypted_key'] = rsa_cipher.decrypt(b64decode(key_to_decrypt['encrypted_key'])).decode('utf-8')
     except:
-        return None
+        if aes_cipher:
+            try:
+                key_to_decrypt['decrypted_key'] = aes_cipher.decrypt(b64decode(key_to_decrypt['encrypted_key'])).decode('utf-8')
+            except:
+                return None
+        else:
+            return None
     return (key_to_decrypt)
 
 def decrypt_all_good_keys(privatekey, v1 = False):
+    redis = get_redis()
+
     key_table = 'saved_session_keys' if v1 else 'saved_session_keys_with_hashes'
     key_der = b64decode(privatekey.strip())
     key_prv = RSA.importKey(key_der)
-    cipher = PKCS1_OAEP.new(key_prv)
+    rsa_cipher = PKCS1_OAEP.new(key_prv)
 
+    aes_cipher = None
+    encrypted_aes_key = redis.get('autoimport-aeskey')
+    if encrypted_aes_key:
+        decrypted_aes_key = rsa_cipher.decrypt(b64decode(encrypted_aes_key))
+        aes_cipher = AES.new(decrypted_aes_key)
+    
     conn = get_raw_conn()
     cursor = conn.cursor()
     cursor.execute(f"SELECT * FROM {key_table} WHERE dead = FALSE")
     encrypted_keys = cursor.fetchall()
     decrypted_keys = []
-    decrypting = Parallel(n_jobs=-1)(delayed(decrypt_key)(key, cipher) for key in encrypted_keys)
+    decrypting = Parallel(n_jobs=-1)(delayed(decrypt_key)(key, rsa_cipher, aes_cipher) for key in encrypted_keys)
     for key in decrypting:
         if key:
             decrypted_keys.append(key)
     return decrypted_keys
 
 def encrypt_and_save_session_for_auto_import(service, key, contributor_id = None, discord_channel_ids = None):
-    key_der = b64decode(config.pubkey.strip())
-    key_pub = RSA.importKey(key_der)
-    cipher = PKCS1_OAEP.new(key_pub)
-    encrypted_bin_key = cipher.encrypt(key.encode())
+    redis = get_redis()
+
+    rsa_key_der = b64decode(config.pubkey.strip())
+    rsa_key_pub = RSA.importKey(key_der)
+    rsa_cipher = PKCS1_OAEP.new(key_pub)
+
+    aes_cipher = None
+    encrypted_aes_key = redis.get('autoimport-aeskey') # base64'd encrypted aes key
+    if not encrypted_aes_key:
+        # create the key, encrypt it with rsa, and save to redis
+        new_aes_key = get_random_bytes(16)
+        aes_cipher = AES.new(key)
+        encrypted_bin_aes_key = rsa_cipher.encrypt(new_aes_key)
+        encrypted_aes_key = b64encode(encrypted_bin_aes_key).decode('utf-8')
+        redis.set('autoimport-aeskey', encrypted_aes_key)
+    else:
+        # decrypt the key
+        decrypted_aes_key = rsa_cipher.decrypt(b64decode(encrypted_aes_key))
+        aes_cipher = AES.new(decrypted_aes_key)
+
+    encrypted_bin_key = aes_cipher.encrypt(key.encode())
     encrypted_key = b64encode(encrypted_bin_key).decode('utf-8')
 
     conn = get_raw_conn()
