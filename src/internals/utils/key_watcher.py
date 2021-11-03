2b9ae1d84a3634b0bb9f7efc555a8e093942284d
Anonymous <>
2021-11-03 02:50:27 -0400

    Key watcher optimizations

--- src/internals/utils/key_watcher.py
+++ src/internals/utils/key_watcher.py
@@ -16,9 +16,19 @@ from src.importers import gumroad
 from src.importers import discord
 from src.importers import fantia
 from setproctitle import setthreadtitle
+from joblib import delayed, Parallel
+
 # a function that first runs existing import requests in a staggered manner (they may be incomplete as importers should delete their keys when they are done) then watches redis for new keys and handles queueing
 # needs to be run in a thread itself
-# remember to clear logs after successful import
+def collect_keys_to_run(key):
+    redis = get_redis()
+    key_data = redis.get(key)
+    if key_data:
+        key_data = json.loads(key_data)
+        if not redis.get(f"running_imports:{archiver_id}:{key_data['import_id']}"):
+            return key_data
+    return None
+
 def watch(queue_limit=2000):
     archiver_id = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(16))
     setthreadtitle(f'Kitsune Key Watcher|{archiver_id}')
@@ -30,67 +40,50 @@ def watch(queue_limit=2000):
             if not thread.is_alive():
                 threads_to_run.remove(thread)
         
-        for key in redis.scan_iter(match='imports:*'):
-            key_data = redis.get(key)
-            if key_data:
-                key_data = json.loads(key_data)
-                if redis.get(f"running_imports:{archiver_id}:{key_data['import_id']}"):
-                    continue
+        keydumps = Parallel(n_jobs=-1)(delayed(collect_keys_to_run)(x) for x in redis.scan_iter(match='imports:*'))
+        for key in keydumps:
+            import_id = key_data['import_id']
+            if key and len(threads_to_run) < queue_limit and filter(lambda thread: thread.name == import_id, threads_to_run) == 0:
+                target = None
+                args = None
+                
+                key = key_data['key']
+                service = key_data['service']
+                allowed_to_auto_import = key_data.get('auto_import', False)
+                allowed_to_save_session = key_data.get('save_session_key', False)
+                allowed_to_scrape_dms = key_data.get('save_dms', False)
+                channel_ids = key_data['channel_ids']
+                contributor_id = key_data['contributor_id']
 
-                if len(threads_to_run) < queue_limit:
-                    target = None
-                    args = None
-                    # data = {
-                    #     'import_id': import_id,
-                    #     'key': key,
-                    #     'service': service,
-                    #     'allowed_to_auto_import': allowed_to_auto_import,
-                    #     'allowed_to_save_session': allowed_to_save_session,
-                    #     'allowed_to_scrape_dms': allowed_to_scrape_dms,
-                    #     'channel_ids': channel_ids,
-                    #     'contributor_id': contributor_id
-                    # }
-                    import_id = key_data['import_id']
-                    key = key_data['key']
-                    service = key_data['service']
-                    allowed_to_auto_import = key_data.get('auto_import', False)
-                    allowed_to_save_session = key_data.get('save_session_key', False)
-                    allowed_to_scrape_dms = key_data.get('save_dms', False)
-                    channel_ids = key_data['channel_ids']
-                    contributor_id = key_data['contributor_id']
-
-                    if key and service and allowed_to_save_session:
-                        try:
-                            encrypt_and_log_session(import_id, service, key)
-                        except:
-                            pass
-                    
-                    if service == 'patreon':
-                        target = patreon.import_posts
-                        args = (key, allowed_to_scrape_dms, contributor_id, allowed_to_auto_import, None)
-                    elif service == 'fanbox':
-                        target = fanbox.import_posts
-                        args = (key, contributor_id, allowed_to_auto_import, None)
-                    elif service == 'subscribestar':
-                        target = subscribestar.import_posts
-                        args = (key, contributor_id, allowed_to_auto_import, None)
-                    elif service == 'gumroad':
-                        target = gumroad.import_posts
-                        args = (key, contributor_id, allowed_to_auto_import, None)
-                    elif service == 'fantia':
-                        target = fantia.import_posts
-                        args = (key, contributor_id, allowed_to_auto_import, None)
-                    elif service == 'discord':
-                        target = discord.import_posts
-                        args = (key, channel_ids.strip().replace(" ", ""), contributor_id, allowed_to_auto_import, None)
-
-                    if target is not None and args is not None:
-                        logger.log(import_id, f'Starting import. Your import id is {import_id}.')
-                        thread = FlaskThread(target=import_posts, args=(import_id, target, args))
-                        thread.start()
-                        threads_to_run.append(thread)
-                        redis.set(f"running_imports:{archiver_id}:{key_data['import_id']}", '1')
-                    else:
-                        logger.log(import_id, f'Error starting import. Your import id is {import_id}.')
-        
-        time.sleep(1)
+                if key and service and allowed_to_save_session:
+                    try:
+                        encrypt_and_log_session(import_id, service, key)
+                    except:
+                        pass
+                
+                if service == 'patreon':
+                    target = patreon.import_posts
+                    args = (key, allowed_to_scrape_dms, contributor_id, allowed_to_auto_import, None)
+                elif service == 'fanbox':
+                    target = fanbox.import_posts
+                    args = (key, contributor_id, allowed_to_auto_import, None)
+                elif service == 'subscribestar':
+                    target = subscribestar.import_posts
+                    args = (key, contributor_id, allowed_to_auto_import, None)
+                elif service == 'gumroad':
+                    target = gumroad.import_posts
+                    args = (key, contributor_id, allowed_to_auto_import, None)
+                elif service == 'fantia':
+                    target = fantia.import_posts
+                    args = (key, contributor_id, allowed_to_auto_import, None)
+                elif service == 'discord':
+                    target = discord.import_posts
+                    args = (key, channel_ids.strip().replace(" ", ""), contributor_id, allowed_to_auto_import, None)
+                
+                if target is not None and args is not None:
+                    thread = FlaskThread(target=import_posts, args=(import_id, target, args))
+                    thread.name = import_id
+                    thread.start()
+                    threads_to_run.append(thread)
+                else:
+                    logger.log(import_id, f'Error starting import. Your import id is {import_id}.')
--- src/lib/import_manager.py
+++ src/lib/import_manager.py
@@ -2,6 +2,8 @@ from ..internals.utils.logger import log
 from ..internals.cache.redis import delete_keys, delete_keys_pattern
 
 def import_posts(import_id, target, args):
+    logger.log(import_id, f'Starting import. Your import id is {import_id}.')
+
     try:
         target(import_id, *args)
     except KeyboardInterrupt:
@@ -13,4 +15,3 @@ def import_posts(import_id, target, args):
     
     # cleanup on "internal" exit
     delete_keys([f'imports:{import_id}'])
-    delete_keys_pattern([f'running_imports:*:{import_id}'])
